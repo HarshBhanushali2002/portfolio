e04551a3c63cf447eb153600e085b678344c3a93
Harsh <bhanushaliharsh1@gmail.com>
2025-11-22 01:42:48 +0530

    implemented hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Welcome from "./components/Welcome";
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Welcome />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
